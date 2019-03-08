e18b4016ff6143eef5a8dbe8c992d16aa53660af
milky5 <sakurasketch@zoho.com>
2019-03-08 18:15:18 +0900

    繰り返しサイコロを振れるようになった。 ただ二回目以降、サイコロボタンを押すだけでサイコロを振ってしまうようになった。

--- Assets/Scriprs/GameScene/BeRolledDice.cs
+++ Assets/Scriprs/GameScene/BeRolledDice.cs
@@ -9,6 +9,7 @@ public class BeRolledDice : MonoBehaviour
     public CharactorStatusKeeper statusKeeper;
     Vector3 dicePosition;
     bool isOnMoveEnd;
+    float realDiceDefaltPosY = 4.0f;
 
     void Start()
     {
@@ -17,6 +18,7 @@ public class BeRolledDice : MonoBehaviour
 
     void Update()
     {
+
         if (isOnMoveEnd) return;
 
         if (statusKeeper.remainMass == 0)
@@ -38,5 +40,10 @@ public class BeRolledDice : MonoBehaviour
     void OnMoveExit()
     {
         fakeDice.SetActive(false);
+        float posX = statusKeeper.playerPos.x;
+        float posY = transform.position.y;
+        float posZ = transform.position.z;
+        gameObject.transform.position = new Vector3(posX, posY, posZ);
+        realDice.transform.position = new Vector3(posX, realDiceDefaltPosY, posZ);
     }
 }
