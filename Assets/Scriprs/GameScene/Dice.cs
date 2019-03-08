688142da29b7a7991cdc0f51da3ba4cf6f8d9640
milky5 <sakurasketch@zoho.com>
2019-03-08 19:45:19 +0900

    サイコロを振った時、UIの[戻る]ボタンを非表示にするように変更

--- Assets/Scriprs/GameScene/Dice.cs
+++ Assets/Scriprs/GameScene/Dice.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Dice : MonoBehaviour
 {
     BeRolledDice beRolledDice;
     int diceNumber;
     Rigidbody rb;
+    [SerializeField] GameObject backButton;
 
     private void Start()
     {
@@ -19,22 +21,51 @@ public class Dice : MonoBehaviour
     {
         transform.Rotate(new Vector3(45, -60, 60) * 7 * Time.deltaTime);
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (rb.useGravity == false)
-            {
-                rb.useGravity = true;
-
-                diceNumber = Random.Range(1, 7);
-                Debug.Log($"出た目は {diceNumber}");
-            }
-        }
-
         if (transform.position.y < 1)
         {
             rb.useGravity = false;
             rb.velocity = Vector3.zero;
             beRolledDice.OnRollingExit(diceNumber);
+            return;
         }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //サイコロがすでに落下をはじめていたら
+            if (rb.useGravity) return;
+
+            //[戻る]ボタンがクリックされているか確認
+            var result = IsUGUIHit(Input.mousePosition);
+
+            //[戻る]ボタンをクリックしていた場合
+            if (result)
+            {
+                gameObject.SetActive(false);
+            }
+
+            //[戻る]ボタンはクリックされておらず、かつサイコロも落下していないとき
+            if (!result && rb.useGravity == false)
+            {
+                //サイコロを落下させる
+                rb.useGravity = true;
+
+                //[戻る]ボタンをfalseに
+                backButton.SetActive(false);
+
+                //サイコロを振る
+                diceNumber = Random.Range(1, 7);
+                Debug.Log($"出た目は {diceNumber}");
+
+            }
+        }
+    }
+
+    public static bool IsUGUIHit(Vector3 _scrPos) // Input.mousePosition
+    {
+        PointerEventData pointer = new PointerEventData(EventSystem.current);
+        pointer.position = _scrPos;
+        List<RaycastResult> result = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointer, result);
+        return (result.Count > 0);
     }
 }
